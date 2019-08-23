cf6959f42637879a6d64f44db5dab0d745465ba6
Jeff Rudd <jeffrudd19@gmail.com>
2019-08-22 20:25:28 -0400

    working 5x5 grid with free space

--- scripts.js
+++ scripts.js
@@ -42,63 +42,146 @@ $(document).ready(function () {
 
         'someone singing',
 
+        'being hit by a backpack',
+
+        'driver does not stop at stop',
+
+        'not being able to fit on board',
+
+        'someone is playing music too loud',
+
+        'backpack taking up a seat',
+
+        'emergency button gets pressed',
+
+        'unexpected detour/delay',
+
+        'someone eating something that smells',
+
+        'witness a dispute',
+
+        'see a cute dog',
+
+        'get a seat with nobody beside you',
+
+        'someone sitting or standing too close',
+
+        'you have to watch a ttc vehicle go past',
+
+        'A presto card is declined',
+
+        'someone is sleeping'
+
     ];
 
     ttcArray = shuffle(ttcArray);
 
-    for (let i = 4; i <= 14; i++) {
-        const spaceAssignment = ttcArray[i-4];
+    for (let i = 6; i <= 30; i++) {
+        const spaceAssignment = ttcArray[i-6];
         $("ul li:nth-child("+ i + ")").text(spaceAssignment);
     }
     //Logic Statements 
+    
     $("li").click(function() {
-      $(this).toggleClass("checked");
+    const Button = $(this).attr("class");
+
+    if (Button.includes("gridItem18") === false) {
+        $(this).toggleClass("checked");
+    }
       if (
-        $(".gridItem4").hasClass("checked") &&
-        $(".gridItem5").hasClass("checked") &&
-        $(".gridItem6").hasClass("checked")
+        $(".gridItem6").hasClass("checked") &&
+        $(".gridItem7").hasClass("checked") &&
+        $(".gridItem8").hasClass("checked") &&
+        $(".gridItem9").hasClass("checked") &&
+        $(".gridItem10").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
+        $(".gridItem11").hasClass("checked") &&
+        $(".gridItem12").hasClass("checked") &&
+        $(".gridItem13").hasClass("checked") &&
+        $(".gridItem14").hasClass("checked") &&
+        $(".gridItem15").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
+        $(".gridItem16").hasClass("checked") &&
+        $(".gridItem17").hasClass("checked") &&
+        $(".gridItem18").hasClass("checked") &&
+        $(".gridItem19").hasClass("checked") &&
+        $(".gridItem20").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
+        $(".gridItem21").hasClass("checked") &&
+        $(".gridItem22").hasClass("checked") &&
+        $(".gridItem23").hasClass("checked") &&
+        $(".gridItem24").hasClass("checked") &&
+        $(".gridItem25").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
+        $(".gridItem26").hasClass("checked") &&
+        $(".gridItem27").hasClass("checked") &&
+        $(".gridItem28").hasClass("checked") &&
+        $(".gridItem29").hasClass("checked") &&
+        $(".gridItem30").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
+        $(".gridItem6").hasClass("checked") &&
+        $(".gridItem11").hasClass("checked") &&
+        $(".gridItem16").hasClass("checked") &&
+        $(".gridItem21").hasClass("checked") &&
+        $(".gridItem26").hasClass("checked")
       ) {
         console.log("You WON");
       } else if (
         $(".gridItem7").hasClass("checked") &&
+        $(".gridItem12").hasClass("checked") &&
+        $(".gridItem17").hasClass("checked") &&
+        $(".gridItem22").hasClass("checked") &&
+        $(".gridItem27").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
         $(".gridItem8").hasClass("checked") &&
-        $(".gridItem9").hasClass("checked")
+        $(".gridItem13").hasClass("checked") &&
+        $(".gridItem18").hasClass("checked") &&
+        $(".gridItem23").hasClass("checked") &&
+        $(".gridItem28").hasClass("checked")
+      ) {
+        console.log("You WON");
+      } else if (
+        $(".gridItem9").hasClass("checked") &&
+        $(".gridItem14").hasClass("checked") &&
+        $(".gridItem19").hasClass("checked") &&
+        $(".gridItem24").hasClass("checked") &&
+        $(".gridItem29").hasClass("checked")
       ) {
         console.log("You WON");
       } else if (
         $(".gridItem10").hasClass("checked") &&
-        $(".gridItem11").hasClass("checked") &&
-        $(".gridItem12").hasClass("checked")
-      ) {
-        console.log("You WON");
-      } else if (
-        $(".gridItem4").hasClass("checked") &&
-        $(".gridItem7").hasClass("checked") &&
-        $(".gridItem10").hasClass("checked")
-      ) {
-        console.log("You WON");
-      } else if (
-        $(".gridItem5").hasClass("checked") &&
-        $(".gridItem8").hasClass("checked") &&
-        $(".gridItem11").hasClass("checked")
+        $(".gridItem15").hasClass("checked") &&
+        $(".gridItem20").hasClass("checked") &&
+        $(".gridItem25").hasClass("checked") &&
+        $(".gridItem30").hasClass("checked")
       ) {
         console.log("You WON");
       } else if (
         $(".gridItem6").hasClass("checked") &&
-        $(".gridItem9").hasClass("checked") &&
-        $(".gridItem12").hasClass("checked")
+        $(".gridItem12").hasClass("checked") &&
+        $(".gridItem18").hasClass("checked") &&
+        $(".gridItem24").hasClass("checked") &&
+        $(".gridItem30").hasClass("checked")
       ) {
         console.log("You WON");
       } else if (
-        $(".gridItem4").hasClass("checked") &&
-        $(".gridItem8").hasClass("checked") &&
-        $(".gridItem12").hasClass("checked")
-      ) {
-        console.log("You WON");
-      } else if (
-        $(".gridItem6").hasClass("checked") &&
-        $(".gridItem8").hasClass("checked") &&
-        $(".gridItem10").hasClass("checked")
+        $(".gridItem10").hasClass("checked") &&
+        $(".gridItem14").hasClass("checked") &&
+        $(".gridItem18").hasClass("checked") &&
+        $(".gridItem22").hasClass("checked") &&
+        $(".gridItem26").hasClass("checked")
       ) {
         console.log("You WON");
       }
